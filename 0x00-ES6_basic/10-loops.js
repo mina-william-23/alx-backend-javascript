c67bce8778d405b8920acba2a9297bfbcf8d6c65
mina-william-23 <capeandroid50@gmail.com>
2024-05-27 16:32:29 +0300

    test

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  const newArray = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
-  
-  return array;
+
+  return newArray;
 }
