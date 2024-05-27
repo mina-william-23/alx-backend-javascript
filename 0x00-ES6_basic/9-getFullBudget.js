1b56a252054db1e4d83f92b587c918825c997624
mina-william-23 <capeandroid50@gmail.com>
2024-05-27 16:27:31 +0300

    test

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,13 +3,13 @@ import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-  ...budget,
-  getIncomeInDollars: function (income) {
-    return `$${income}`;
-  },
-  getIncomeInEuros: function (income) {
-    return `${income} euros`;
-  },
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
   };
 
   return fullBudget;
