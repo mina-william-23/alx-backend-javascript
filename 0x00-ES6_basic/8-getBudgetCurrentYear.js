4366588dade11c23a054da6d95960663f8a243ea
mina-william-23 <capeandroid50@gmail.com>
2024-05-27 16:19:41 +0300

    test

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -2,13 +2,11 @@ function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
-  
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-  
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-  
-  return budget;
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  }
 }
