c398819c9110ae65a9093c8ab6fbac9c91f0ef06
mina-william-23 <capeandroid50@gmail.com>
2024-10-03 18:40:03 +0000

    10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,50 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    if (typeof newBrand !== 'string') {
+      throw TypeError('Brand must be a string');
+    }
+    this._brand = newBrand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    if (typeof newMotor !== 'string') {
+      throw TypeError('Motor must be a string');
+    }
+    this._motor = newMotor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    if (typeof newColor !== 'string') {
+      throw TypeError('Color must be a string');
+    }
+    this._color = newColor;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
